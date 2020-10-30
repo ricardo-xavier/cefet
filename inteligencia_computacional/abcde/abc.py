50cc71a46f79b63e002d7168a84d1dbb2d95411e
ricardo-xavier <ricardo.costa.xavier@gmail.com>
2020-10-29 22:37:39 -0300

    elipse

--- inteligencia_computacional/abcde/abc.py
+++ inteligencia_computacional/abcde/abc.py
@@ -8,31 +8,34 @@ def processa(arquivo):
     #cv2.imshow(arquivo, img);
     #cv2.waitKey()
 
-    # separa a frente(preto) do fundo(branco)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    (thresh, bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    cv2.imshow(arquivo, bw)
+    im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    im_blur = cv2.GaussianBlur(im_gray, (3, 3), 0)
+    ret, th = cv2.threshold(im_blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    imgEdge, contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    #image = cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
 
-    # retorna os segmentos da imagem
-    num_segments = seed_growing_c(bw, bw.shape[1], bw.shape[0])
-    print(num_segments)
+    bigger_idx = 0
+    bigger_len = 0
+    for idx in range(0, len(contours)):
+        cont = contours[idx]
+        cont_len = len(cont)
+        if cont_len > bigger_len:
+            bigger_idx = idx
+            bigger_len = cont_len
 
+    cont = contours[bigger_idx]
+    elps = cv2.fitEllipse(cont)
+    image = cv2.drawContours(img, [ cont ], -1, (0, 0, 255), 2)
+    cv2.ellipse(image, elps, (0, 255, 0), 2)
+    cv2.imshow(arquivo, image);
     cv2.waitKey()
     return
 
-'''
-for i in range(1, 11):
+#'''
+for i in range(1, 10):
     for tipo in [ "b", "m" ]:
         nome = "imagens/" + tipo + str(i) + ".jpg"
         processa(nome)
-'''
+#'''
 
-lib = ctypes.cdll.LoadLibrary("./libabc.so")
-seed_growing_c = lib.seed_growing
-seed_growing_c.restype = int
-seed_growing_c.argtypes = [
-    ndpointer(ctypes.c_uint8, flags="C_CONTIGUOUS"), # image
-    ctypes.c_int,                                    # width
-    ctypes.c_int ]                                   # height
-
-processa("imagens/b1.jpg")
+#processa("imagens/b1.jpg")
